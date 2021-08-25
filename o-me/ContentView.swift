06ff88c24ec9c951cbb9ee42ce984ee8a30a63a1
Jake-Sturgeon <sturgeonjake52@gmail.com>
2021-08-25 20:29:39 +0100

    rename

---
+++ o-me/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  o-me
+//
+//  Created by Jake Sturgeon on 25/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ o-me/o_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  o_meApp.swift
+//  o-me
+//
+//  Created by Jake Sturgeon on 25/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct o_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
